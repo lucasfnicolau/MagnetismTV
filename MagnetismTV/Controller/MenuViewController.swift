36a3e57a71175b26b88029f06a35f060aa0be4e9
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-07-17 15:13:20 -0300

    Criação de mais um level; Facilitação na definição de Score e TimeLimit de cada fase (via .sks); Correção de assets dos Tiles;

---
+++ MagnetismTV/Controller/MenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MenuViewController.swift
+//  MagnetismTV
+//
+//  Created by Lucas Fernandez Nicolau on 16/07/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var musicControlButton: UIButton!
+    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var selectLevelButton: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
