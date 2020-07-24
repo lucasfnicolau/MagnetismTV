40fb61180b7db5b90ee754ef54759598eab5a048
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-07-24 17:27:28 -0300

    Novos levels, com um sendo o tutorial. Player agora muda o lado que está "olhando".

--- MagnetismTV/Controller/GameViewController.swift
+++ MagnetismTV/Controller/GameViewController.swift
@@ -13,8 +13,8 @@ import GameplayKit
 class GameViewController: UIViewController {
 
     private var timerScoreView: TimerScoreView?
-    private var currentLevel = 0
     private var currentScene: Level?
+    var currentLevel = 0
 
     private(set) static var isPaused = false
 
@@ -36,20 +36,20 @@ class GameViewController: UIViewController {
     func start(sceneWithIndex index: Int) {
         currentLevel = index
         stop()
-        let name = formattedLevelName(forIndex: index)
+        let name = GameViewController.formattedLevelName(forIndex: index)
         self.currentScene = createScene(named: name)
         guard let currentScene = currentScene else { return }
         setupView(for: currentScene)
     }
 
 
-    private func checkExistenceOf(sceneAtIndex index: Int) -> Bool {
+    static func doesSceneExists(atIndex index: Int) -> Bool {
         let name = formattedLevelName(forIndex: index)
         return SKScene(fileNamed: name) != nil
     }
 
 
-    private func formattedLevelName(forIndex index: Int) -> String {
+    static func formattedLevelName(forIndex index: Int) -> String {
         let name: String
         if index >= 100 {
             name = "Level\(index)"
