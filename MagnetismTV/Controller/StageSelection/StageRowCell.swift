6d9e22e50bf80d028f722ee570cec32343486305
Rafael Galdino <galdineris@gmail.com>
2020-07-28 13:07:09 -0300

    Refactor View Composition + Add Stage Selection Logic

---
+++ MagnetismTV/Controller/StageSelection/StageRowCell.swift
@@ -0,0 +1,13 @@
+//
+//  StageRowCell.swift
+//  MagnetismTV
+//
+//  Created by Rafael Galdino on 24/07/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class StageRowCell: UICollectionViewCell {
+    @IBOutlet weak var SelectionButton: CustomButton!
+}
