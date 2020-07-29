d7358e1f0ca0cf0232227ffc68309db52e53c655
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-07-29 17:40:34 -0300

    Função para sekecionar o nível pronta

---
+++ MagnetismTV/Model/StageSelectionModel.swift
@@ -0,0 +1,9 @@
+//
+//  StageSelectionModel.swift
+//  MagnetismTV
+//
+//  Created by Lucas Fernandez Nicolau on 29/07/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import Foundation
